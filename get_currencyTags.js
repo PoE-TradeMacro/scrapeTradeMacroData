cfb223233e3ba9e051cb29d3d9a0b73ad9ed4725
Eruyome <prinzeru@gmail.com>
2018-05-13 02:15:22 +0200

    added some short names to currency tags

--- get_currencyTags.js
+++ get_currencyTags.js
@@ -30,6 +30,20 @@ request(options, function (error, response, body) {
 	for (var type in cTags.tags) {
 		for (var e in cTags.tags[type]) {
 			delete cTags.tags[type][e].image;
+			
+			var reg_whiteSextant = /Apprentice Cartographer's Sextant/i;
+			var reg_yellowSextant = /Journeyman Cartographer's Sextant/i;
+			var reg_redSextant = /Master Cartographer's Sextant/i;
+			
+			var text = cTags.tags[type][e].text;
+			
+			if (text.match(reg_whiteSextant)) {
+				cTags.tags[type][e].short = "Apprentice Sextant (white)";
+			} else if (text.match(reg_yellowSextant)) {
+				cTags.tags[type][e].short = "Journeyman Sextant (yellow)";
+			} else if (text.match(reg_redSextant)) {
+				cTags.tags[type][e].short = "Master Sextant (red)";
+			}
 		}
 	}	
 	//console.log(util.inspect(cTags, false, null));
