633f518797dfe345fc84c590ca2ab29dde1a98c9
Eruyome <prinzeru@gmail.com>
2019-12-18 18:55:45 +0100

    updates

--- compare.js
+++ compare.js
@@ -30,14 +30,22 @@ function copyAndCompareFiles() {
 		console.log('Scrape output folder: "' + outPutDir + '".');
 		console.log('Temp compare folder : "' + tempFolder + '".');
 		
-		files = ['mods.json', 'uniques.json'];
-		for (i = 0; i < files.length; i++) {
-			var tmFile = jsonfile.readFileSync(TMDir + '\\' + files[i]);
-			var scFile = jsonfile.readFileSync(outPutDir + '\\' + files[i]);
-			jsonfile.writeFileSync(tempFolder + '\\' + files[i], tmFile, {spaces: 2});
-			jsonfile.writeFileSync(tempFolder + '\\' + 'scraped_' + files[i], scFile, {spaces: 2});			
+		jsonfiles = ['mods.json', 'uniques.json', 'item_bases_armour.json', 'item_bases_weapon.json', 'item_bases.json','currency_tags.json'];
+		for (i = 0; i < jsonfiles.length; i++) {
+			var tmFile = jsonfile.readFileSync(TMDir + '\\' + jsonfiles[i]);
+			var scFile = jsonfile.readFileSync(outPutDir + '\\' + jsonfiles[i]);
+			jsonfile.writeFileSync(tempFolder + '\\' + jsonfiles[i], tmFile, {spaces: 2});
+			jsonfile.writeFileSync(tempFolder + '\\' + 'scraped_' + jsonfiles[i], scFile, {spaces: 2});
 		}
-		
+
+		txtfiles = ['glove_enchantment_mods.txt', 'helmet_enchantment_mods.txt' , 'boot_enchantment_mods.txt', 'item_corrupted_mods.txt'];
+		for (i = 0; i < txtfiles.length; i++) {
+			var tmFile = fs.readFileSync(TMDir + '\\' + txtfiles[i]);
+			var scFile = fs.readFileSync(outPutDir + '\\' + txtfiles[i]);
+			fs.writeFileSync(tempFolder + '\\' + txtfiles[i], tmFile, {spaces: 2});
+			fs.writeFileSync(tempFolder + '\\' + 'scraped_' + txtfiles[i], scFile, {spaces: 2});
+		}
+
 		sleep.sleep(1)
 		// open temporary folder in windows explorer
 		// require('child_process').exec('start "" "' + tempFolder + '"');
@@ -45,12 +53,52 @@ function copyAndCompareFiles() {
 		// open mods.json files in winmerge
 		var left = tempFolder + '\\mods.json';
 		var right = tempFolder + '\\scraped_mods.json';
-		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "mods old" /dr "mods new" "' + left + '" "' + right + '"');
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "mods old" /dr "new" "' + left + '" "' + right + '"');
 		
 		// open uniques.json files in winmerge
 		left = tempFolder + '\\uniques.json';
 		right = tempFolder + '\\scraped_uniques.json';
-		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "uniques old" /dr "uniques new" "' + left + '" "' + right + '"');
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "uniques old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open helmet_enchantment_mods.txt files in winmerge
+		left = tempFolder + '\\helmet_enchantment_mods.txt';
+		right = tempFolder + '\\scraped_helmet_enchantment_mods.txt';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "helmet enchants old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open glove_enchantment_mods.txt files in winmerge
+		left = tempFolder + '\\glove_enchantment_mods.txt';
+		right = tempFolder + '\\scraped_glove_enchantment_mods.txt';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "glove enchants old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open boot_enchantment_mods.txt files in winmerge
+		left = tempFolder + '\\boot_enchantment_mods.txt';
+		right = tempFolder + '\\scraped_boot_enchantment_mods.txt';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "boot enchants old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open item_corrupted_mods.txt files in winmerge
+		left = tempFolder + '\\item_corrupted_mods.txt';
+		right = tempFolder + '\\scraped_item_corrupted_mods.txt';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "corrupted mods old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open currency_tags.json files in winmerge
+		left = tempFolder + '\\currency_tags.json';
+		right = tempFolder + '\\scraped_currency_tags.json';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "currency_tags old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open item_bases_armour.json files in winmerge
+		left = tempFolder + '\\item_bases_armour.json';
+		right = tempFolder + '\\scraped_item_bases_armour.json';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "armour old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open item_bases_weapon.json files in winmerge
+		left = tempFolder + '\\item_bases_weapon.json';
+		right = tempFolder + '\\scraped_item_bases_weapon.json';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "weapon old" /dr "new" "' + left + '" "' + right + '"');
+
+		// open item_bases.json files in winmerge
+		left = tempFolder + '\\item_bases.json';
+		right = tempFolder + '\\scraped_item_bases.json';
+		require('child_process').exec('start winmerge.exe /s /u /maximize /dl "bases old" /dr "new" "' + left + '" "' + right + '"');
 	}
 }
 
